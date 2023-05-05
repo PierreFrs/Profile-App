1570192c2cdc12d0ec46cbc7c12b0634e1f88a96
Pierre <pierrefraisse90@gmail.com>
2023-05-05 19:05:35 +0200

    search component

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -5,16 +5,37 @@ import {FaTrashAlt} from "react-icons/fa";
 
 const Profile = () => {
     const [userProfile, setUserProfile] = useState(profileData);
+    const [search, setSearch] = useState('');
 
     const removeProfile = (id) => {
         const newProfileList = userProfile.filter((profile) => profile.id !== id);
         setUserProfile(newProfileList)
     }
+
+    const handleInputChange = e => {
+        setSearch(e.target.value)
+    }
+
   return (
     <section className="profile-sec --center-all">
         <div className="container">
             <h2 className='--text-light'>Profile App</h2>
-            {userProfile.map((profile) => (
+            <div className="--form-control">
+                <input 
+                type="text" 
+                placeholder='Enter search term...' 
+                className='--width-100'
+                onChange={handleInputChange}
+                value={search}
+                />
+            </div>
+            {userProfile.filter((value) => {
+                if(search === '') {
+                    return value;
+                } else if (value.name.toLowerCase().includes(search.toLowerCase())){
+                    return value
+                }
+            }).map((profile) => (
                 <div className="profile --card --flex-between --p" key={profile.id}>
                     <img src={profile.img} alt="profile picture" />
                     <div className="description">
