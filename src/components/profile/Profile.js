4b388ab39622f522f09c4a6e1ba109b7c751e848
Pierre <pierrefraisse90@gmail.com>
2023-05-05 18:52:02 +0200

    logic

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -4,19 +4,24 @@ import {profileData} from './profile-data';
 import {FaTrashAlt} from "react-icons/fa";
 
 const Profile = () => {
-    const [userProfile, setUserProfile] = useState(profileData)
+    const [userProfile, setUserProfile] = useState(profileData);
+
+    const removeProfile = (id) => {
+        const newProfileList = userProfile.filter((profile) => profile.id !== id);
+        setUserProfile(newProfileList)
+    }
   return (
-    <section className="profile-sec">
+    <section className="profile-sec --center-all">
         <div className="container">
             <h2 className='--text-light'>Profile App</h2>
             {userProfile.map((profile) => (
                 <div className="profile --card --flex-between --p" key={profile.id}>
                     <img src={profile.img} alt="profile picture" />
                     <div className="description">
-                        <h4 className='--text-light'>Name: {profile.name}</h4>
-                        <p className='--text-light'>Job: {profile.job}</p>
+                        <h4 className='--text-light'>{profile.name}</h4>
+                        <p className='--text-light'>{profile.job}</p>
                     </div>
-                    <FaTrashAlt size={18} className='icon' />
+                    <FaTrashAlt size={18} className='icon' onClick={() => removeProfile(profile.id)}/>
                 </div>
             ))}
         </div> 
